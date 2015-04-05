e4a36e25b8f73101acccedb982fd89412f558514
rafiq1412 <rivalgen@gmail.com>
2015-04-05 15:39:41 -0700

    Version 3

---
+++ MobileApp/app/src/main/java/com/android/cse360/mobileapp/Login.java
@@ -0,0 +1,11 @@
+package com.android.cse360.mobileapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.*;
+
+public class Login extends Activity {
+
+}
